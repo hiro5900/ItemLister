4a4c446137b3ea50d04c3c9b5a8fa317a8d713ec
hiro5900 <achyut5900@gmail.com>
2023-05-19 20:24:25 +0530

    Updated script.js and modified the styles of the list items.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-// let headerTitle = document.getElementById('header-title');
-// let header = document.getElementById('main-header')
-// console.log(headerTitle)
-// // headerTitle.textContent = 'Hello'
-// // headerTitle.innerText = 'Goodbye'
-// header.style.border = '3px solid #000'
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header')
+console.log(headerTitle)
+// headerTitle.textContent = 'Hello'
+// headerTitle.innerText = 'Goodbye'
+header.style.border = '3px solid #000'
 
-// let items = document.getElementsByClassName('list-group-item')
-// console.log(items)
-// console.log(items[1])
-// items[1].textContent = 'Hello 2'
-// items[1].style.fontWeight = 'bold'
-// items[1].style.backgroundColor = 'yellow'
+let items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[1].textContent = 'Hello 2'
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+items[2].style.backgroundColor = 'green'
+for (let i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold'
+}
 
-// document.getElementsByClassName('title')[0].style.fontWeight = 'bold'
-// document.getElementsByClassName('title')[0].style.color = 'green'
+document.getElementsByClassName('title')[0].style.fontWeight = 'bold'
+document.getElementsByClassName('title')[0].style.color = 'green'
+
+let addItem = document.getElementsByClassName('item')
+console.log(addItem, typeof addItem)
