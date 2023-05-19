765a25ece2186d3a577136f414355f329c7ef8f6
hiro5900 <achyut5900@gmail.com>
2023-05-19 19:56:47 +0530

    Added two files index.html and script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header')
+// console.log(headerTitle)
+// // headerTitle.textContent = 'Hello'
+// // headerTitle.innerText = 'Goodbye'
+// header.style.border = '3px solid #000'
+
+// let items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent = 'Hello 2'
+// items[1].style.fontWeight = 'bold'
+// items[1].style.backgroundColor = 'yellow'
+
+document.getElementsByClassName('title')[0].style.fontWeight = 'bold'
+document.getElementsByClassName('title')[0].style.color = 'green'
