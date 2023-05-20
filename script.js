fe214ccb9cd655c64d0a12a481afa0304cfbbb6a
hiro5900 <achyut5900@gmail.com>
2023-05-21 03:13:56 +0530

    Updated index.html & script.js as well as added functions to add & delete items.

--- script.js
+++ script.js
@@ -1,100 +1,58 @@
-// // let headerTitle = document.getElementById('header-title');
-// // let header = document.getElementById('main-header')
-// // console.log(headerTitle)
-// // // headerTitle.textContent = 'Hello'
-// // // headerTitle.innerText = 'Goodbye'
-// // header.style.border = '3px solid #000'
-
-// let items = document.getElementsByClassName('list-group-item')
-// console.log(items)
-// console.log(items[1])
-// items[1].textContent = 'Hello 2'
-// items[1].style.fontWeight = 'bold'
-// // items[1].style.backgroundColor = 'green'
-// items[2].style.backgroundColor = 'green'
-// for (let i = 0; i < items.length; i++) {
-//     items[i].style.fontWeight = 'bold'
-// }
-
-// document.getElementsByClassName('title')[0].style.fontWeight = 'bold'
-// document.getElementsByClassName('title')[0].style.color = 'green'
-
-// // document.getElementsByTagName('li')[4].style.backgroundColor = 'red'
-// document.getElementsByClassName('list-group-item')[4].style.backgroundColor = 'red'
-
-// let header = document.querySelector('#main-header')
-// header.style.borderBottom = '4px solid #ccc'
-
-// let titles = document.querySelectorAll('.title')
-
-// document.querySelectorAll('.list-group-item')[1].style.color = 'green'
-
-// let odd = document.querySelectorAll('li:nth-child(odd)')
-// let even = document.querySelectorAll('li:nth-child(even)')
-
-// for (let i = 0; i < odd.length; i++) {
-//     odd[i].style.backgroundColor = 'green' 
-//     even[i].style.backgroundColor = 'aqua' 
-// }
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items')
+let filter = document.getElementById('filter')
 
 
-// Traversing DOM
-let itemList = document.querySelector('#items')
-// parentNode
-// console.log(itemList.parentNode)
-// itemList.parentNode.style.backgroundColor = 'gray'
-// console.log(itemList.parentNode.parentNode)
+let addition = (e) => {
+     e.preventDefault();
+     
+     let newItem = document.getElementById('item').value
+     let li = document.createElement('li')
+     li.className = 'list-group-item'
+     li.appendChild(document.createTextNode(newItem))
 
-// parentElement
-// console.log(itemList.parentElement)
-// itemList.parentElement.style.backgroundColor = 'gray'
-// console.log(itemList.parentElement.parentElement)
+     let deleteBtn = document.createElement('button')
+     deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
+     deleteBtn.appendChild(document.createTextNode('X'))
 
-// childNodes
-// console.log(itemList.childNodes)
-// console.log(itemList.children)
-// itemList.children[1].style.backgroundColor = 'lightpink'
+     let editBtn = document.createElement('button')
+     editBtn.className = 'btn btn-sm float-right'
+     editBtn.appendChild(document.createTextNode('Edit'))
 
-// firstChild
-// console.log(itemList.firstChild)
-// firstElementChild
-// console.log(itemList.firstElementChild)
-// itemList.firstElementChild.textContent = 'Hello 1'
+     li.appendChild(editBtn)
 
-// nextSibling
-// console.log(itemList.nextSibling)
-// nextElementSibling
-// console.log(itemList.nextElementSibling)
+     li.appendChild(deleteBtn)
 
-// previousSibling
-// console.log(itemList.previousSibling)
-// previousElementSibling
-// console.log(itemList.previousElementSibling)
-// itemList.previousElementSibling.style.color = 'purple'
+     itemList.appendChild(li)
+}
+form.addEventListener('submit', addition)
 
-// createElement
-let newDiv = document.createElement('div')
+let removeItem = (e) => {
+     if(e.target.classList.contains('delete')){
+          if(confirm('Are you sure ?')){
+               let li = e.target.parentElement
+               itemList.removeChild(li)
+          }
+     }
+}
 
-// add class
-newDiv.className = 'hello'
-// add id
-newDiv.id = 'hello1'
-// add attribute
-newDiv.setAttribute('title', 'Hello div')
-// create text node
-let newDivText = document.createTextNode('Hello World')
-// add text to div
-newDiv.appendChild(newDivText)
+itemList.addEventListener('click', removeItem)
 
-let container = document.querySelector('header .container')
-let h1 = document.querySelector('header h1')
-console.log(newDiv)
+let filterItems = (e) => {
+     let text = e.target.value.toLowerCase()
+     let items = itemList.getElementsByTagName('li')
 
-container.insertBefore(newDiv, h1)
+     Array.from(items).forEach((item) => {
+          let itemName = item.firstChild.textContent
+          if(itemName.toLowerCase().indexOf(text) != -1){
+               item.style.display = 'block'
+          }
+          else{
+               item.style.display = 'none'
+          }
+     })
+}
 
-let x = document.getElementById('items')
-let y = document.createElement('h2')
-y.className = 'greet'
-y.textContent = 'Hello World'
+filter.addEventListener('keyup', filterItems)
 
-x.insertBefore(y, x.firstChild)
+
