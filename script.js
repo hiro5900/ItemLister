36ee0e2ab4ea4c6f3da289f8b29d24619c0030dd
hiro5900 <achyut5900@gmail.com>
2023-05-21 19:29:25 +0530

    Added a description input field in index.html

--- script.js
+++ script.js
@@ -4,55 +4,57 @@ let filter = document.getElementById('filter')
 
 
 let addition = (e) => {
-     e.preventDefault();
-     
-     let newItem = document.getElementById('item').value
-     let li = document.createElement('li')
-     li.className = 'list-group-item'
-     li.appendChild(document.createTextNode(newItem))
+  e.preventDefault();
 
-     let deleteBtn = document.createElement('button')
-     deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
-     deleteBtn.appendChild(document.createTextNode('X'))
+  let newItem = document.getElementById('item').value
+  let li = document.createElement('li')
+  li.className = 'list-group-item'
+  li.appendChild(document.createTextNode(newItem))
 
-     let editBtn = document.createElement('button')
-     editBtn.className = 'btn btn-sm float-right'
-     editBtn.appendChild(document.createTextNode('Edit'))
+  let desc = document.getElementById('desc').value
+  li.appendChild(document.createTextNode(' ' + desc))
 
-     li.appendChild(editBtn)
+  let deleteBtn = document.createElement('button')
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
+  deleteBtn.appendChild(document.createTextNode('X'))
 
-     li.appendChild(deleteBtn)
+  let editBtn = document.createElement('button')
+  editBtn.className = 'btn btn-sm float-right'
+  editBtn.appendChild(document.createTextNode('Edit'))
 
-     itemList.appendChild(li)
+  li.appendChild(editBtn)
+
+  li.appendChild(deleteBtn)
+
+  itemList.appendChild(li)
 }
 form.addEventListener('submit', addition)
 
 let removeItem = (e) => {
-     if(e.target.classList.contains('delete')){
-          if(confirm('Are you sure ?')){
-               let li = e.target.parentElement
-               itemList.removeChild(li)
-          }
-     }
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are you sure ?')) {
+      let li = e.target.parentElement
+      itemList.removeChild(li)
+    }
+  }
 }
 
 itemList.addEventListener('click', removeItem)
 
 let filterItems = (e) => {
-     let text = e.target.value.toLowerCase()
-     let items = itemList.getElementsByTagName('li')
+  let text = e.target.value.toLowerCase()
+  let items = itemList.getElementsByTagName('li')
 
-     Array.from(items).forEach((item) => {
-          let itemName = item.firstChild.textContent
-          if(itemName.toLowerCase().indexOf(text) != -1){
-               item.style.display = 'block'
-          }
-          else{
-               item.style.display = 'none'
-          }
-     })
+  Array.from(items).forEach((item) => {
+    let itemName = item.firstChild.textContent
+    let descName = item.childNodes[1].textContent
+    if (itemName.toLowerCase().indexOf(text) != -1 || descName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block'
+    }
+    else {
+      item.style.display = 'none'
+    }
+  })
 }
 
 filter.addEventListener('keyup', filterItems)
-
-
